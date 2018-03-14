f33ea852d44cf44652ecb17c41cf8d09188d1166
Kevin LARQUEMIN <kevin.larquemin@continental-corporation.com>
2018-03-14 14:11:24 +0100

    upgrade travis jobs to generate and publish bottle

---
+++ darwin-xnu/homebrew/redsocks2.rb
@@ -0,0 +1,78 @@
+require 'formula'
+
+class GitNoDepthDownloadStrategy < GitDownloadStrategy
+  # We need the .git folder for it's information, so we clone the whole thing
+  def stage
+    dst = Dir.getwd
+    @clone.cd do
+      reset
+      safe_system 'git', 'clone', '.', dst
+    end
+  end
+end
+
+class Redsocks2 < Formula
+  desc "Transparent socks redirector"
+  homepage "https://github.com/VixsTy/redsocks"
+  version "{{tags}}"
+
+  stable do
+    url 'https://github.com/VixsTy/redsocks.git',
+      :using => GitNoDepthDownloadStrategy, :shallow => false, :tag => "{{tags}}"
+  end
+
+  head do
+    url 'https://github.com/VixsTy/redsocks.git',
+      :using => GitNoDepthDownloadStrategy, :shallow => false
+  end
+
+  bottle do
+    root_url "https://github.com/VixsTy/redsocks/releases/download/{{tags}}"
+    cellar :any
+  end
+
+
+  depends_on "openssl"
+  depends_on "libevent"
+
+  def install
+    system "git submodule update --init darwin-xnu/$(sw_vers -productVersion | cut -d '.' -f 1,2)"
+    system "make"
+    bin.install "redsocks2"
+    man8.install "debian/redsocks.8"
+    etc.install "darwin-xnu/redsocks.conf"
+  end
+
+  def caveats
+    "Edit #{etc}/redsocks.conf to configure redsocks"
+  end
+
+  plist_options :startup => true
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/redsocks2</string>
+          <string>-c</string>
+          <string>#{etc}/redsocks.conf</string>
+        </array>
+        <key>KeepAlive</key>
+        <false/>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>StandardErrorPath</key>
+        <string>/dev/null</string>
+        <key>StandardOutPath</key>
+        <string>/dev/null</string>
+      </dict>
+    </plist>
+    EOS
+  end
+
+end
